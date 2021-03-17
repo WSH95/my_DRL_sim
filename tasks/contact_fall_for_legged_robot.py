dbc2699e49bfb4a164e8edb092d33ba3ef73f898
WSH95 <d201780203@hust.edu.cn>
2021-03-17 13:59:49 +0800

    Change the Contact Detection Function to Class

--- tasks/contact_fall_for_legged_robot.py
+++ tasks/contact_fall_for_legged_robot.py
@@ -21,3 +21,36 @@ def is_contact_fall(env):
             contact_fall = True
 
     return contact_fall
+
+
+class ContactDetection:
+    def __init__(self, env):
+        self._env = env
+        self._pybullet_client = self._env.getClient()
+        self._robotID = None
+        self._groundID = None
+        self._foot_links = None
+
+    def reset(self):
+        self._robotID = self._env.robot.getRobotID()
+        self._groundID = self._env.ground
+        self._foot_links = self._env.robot.GetFootLinkIDs()
+
+    def is_contact_fall(self):
+        contact_fall = False
+
+        if self._env.env_step_counter > 0:
+            robot_ground_contact_points = self._pybullet_client.getContactPoints(self._robotID, bodyB=self._groundID)
+            robot_self_contact_points = self._pybullet_client.getContactPoints(bodyA=self._robotID, bodyB=self._robotID)
+            if robot_ground_contact_points:
+                for contact_point in robot_ground_contact_points:
+                    if contact_point[3] not in self._foot_links:
+                        print("Ground contact!")
+                        contact_fall = True
+                        break
+
+            if robot_self_contact_points:
+                print("Self contact!")
+                contact_fall = True
+
+        return contact_fall
