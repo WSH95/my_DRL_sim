bd6fb2674088d955cc5e3841e91f38912daa1f86
WSH95 <d201780203@hust.edu.cn>
2021-03-17 13:53:41 +0800

    Add parameter num_action_repeat

--- envs/locomotion_gym_config.py
+++ envs/locomotion_gym_config.py
@@ -3,15 +3,16 @@ import attr
 
 @attr.s
 class SimulationParameters(object):
-    enable_rendering = attr.ib(type=bool, default=True)  # whether to show the interface. COV_ENABLE_RENDERING
-    enable_rendering_gui = attr.ib(type=bool, default=True)  # COV_ENABLE_GUI
+    enable_rendering = attr.ib(type=bool, default=False)  # whether to show the interface. COV_ENABLE_RENDERING
+    enable_rendering_gui = attr.ib(type=bool, default=False)  # COV_ENABLE_GUI
     camera_distance = attr.ib(type=float, default=1.0)
     camera_yaw = attr.ib(type=float, default=0)
     camera_pitch = attr.ib(type=float, default=-30)
     render_width = attr.ib(type=int, default=480)
     render_height = attr.ib(type=int, default=360)
-    egl_rendering = attr.ib(type=bool, default=False)  # enable hardware accelerated OpenGL rendering without a X11 context
+    egl_rendering = attr.ib(type=bool, default=True)  # enable hardware accelerated OpenGL rendering without a X11 context
     enable_hard_reset = attr.ib(type=bool, default=False)
     reset_time = attr.ib(type=float, default=-1)
     time_step = attr.ib(type=float, default=1.0 / 240.0)
+    num_action_repeat = attr.ib(type=int, default=1)
     train_or_test = attr.ib(type=str, default="train")
